d516728e28ced9644f0af1734f110ef065eefd14
Nathan Faubion <nathan@n-son.com>
2012-09-29 14:12:39 -0400

    Updated examples

--- examples/list.js
+++ examples/list.js
@@ -1,13 +1,24 @@
 var adt = require("../adt");
 
-var List = adt.data(function (list, type) {
-  type('Empty');
-  type('Cons', function (cons, field) {
-    field('head');
-    field('tail', adt.only(list));
-  });
+var List = adt.data(function () {
+  this.apply = function (ctx, args) {
+    return List.fromArray(args);
+  };
 });
 
+var Empty = List.type("Empty");
+var Cons  = List.type("Cons", { head: adt.any, tail: adt.only(List) });
+
+// Create a list from an array
+List.fromArray = function (arr) {
+  var i = arr.length, l = Empty();
+  while (i--) {
+    l = Cons(arr[i], l);
+  }
+  return l;
+};
+
+// Make it foldable
 List.prototype.foldl = function (fn, memo) {
   var item = this;
   while (item.isCons()) {
@@ -15,15 +26,6 @@ List.prototype.foldl = function (fn, memo) {
     item = item.tail();
   }
   return memo;
-};
-
-// Create a list from an array
-List.fromArray = function (arr) {
-  var i = arr.length, l = List.Empty();
-  while (i--) {
-    l = List.Cons(arr[i], l);
-  }
-  return l;
 };
 
 // Create an array from a list
@@ -34,40 +36,37 @@ List.prototype.toArray = function () {
   }, []);
 };
 
-// Custom toString method
-List.prototype.toString = function () {
-  return this.isEmpty() ? "Empty" : "Cons(" + this.head() + ", " + this.tail().toString() + ")";
-};
-
 // Find the length of the list
 List.prototype.length = function () {
-  return this.foldl(function (val, len) {
+  return this.foldl(function (len, val) {
     return len + 1;
   }, 0);
 };
 
 // Concat two lists together
 List.prototype.concat = function (list) {
-  return this.isEmpty() ? list : List.Cons(this.head(), this.tail().concat(list));
+  return this.isEmpty() ? list : Cons(this.head(), this.tail().concat(list));
 };
 
 // Flatten a level
 List.prototype.flatten = function () {
   return this.foldl(function (memo, list) {
     return memo.concat(list);
-  }, List.Empty());
+  }, Empty());
 };
 
 // Make it a functor
 List.prototype.map = function (fn) {
-  return this.isEmpty() ? this : List.Cons(fn(this.head()), this.tail().map(fn));
+  return this.isEmpty() ? this : Cons(fn(this.head()), this.tail().map(fn));
 };
 
 // Make it a monad
 List.unit = function (val) {
-  return List.Cons(val, List.Empty());
+  return Cons(val, Empty());
 };
 
 List.prototype.then = function (fn) {
   return this.isEmpty() ? this : this.map(fn).flatten();
 };
+
+module.exports = List;
--- examples/maybe.js
+++ examples/maybe.js
@@ -1,19 +1,22 @@
-var Maybe = adt.data({
-  Nothing : adt.single(),
-  Just    : adt.record("val")
-});
+var adt = require("../adt");
+
+var Maybe   = adt.data();
+var Nothing = Maybe.type("Nothing");
+var Just    = Maybe.type("Just", { val: adt.any });
 
 // Make it a functor.
 Maybe.prototype.map = function (fn) {
-  return this.isNothing() ? this : Maybe.Just(fn(this.val()));
+  return this.isNothing() ? this : Just(fn(this.val()));
 };
 
 // Make it a monad.
 Maybe.unit = function (val) {
-  return Maybe.Just(val);
+  return Just(val);
 };
 
 // Call bind `then` so that it doesn't clash with the semantics of js bind
 Maybe.prototype.then = function (fn) {
   return this.isNothing() ? this : fn(this.val());
 };
+
+module.exports = Maybe;

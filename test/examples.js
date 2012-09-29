d1d9e526f6630a8f764498b28ffc71b7e643b25e
Nathan Faubion <nathan@n-son.com>
2012-09-29 14:21:01 -0400

    Updated tests

--- test/examples.js
+++ test/examples.js
@@ -15,15 +15,15 @@ suite("Examples", function () {
     var just = Maybe.Just(42);
     var noth = Maybe.Nothing();
 
-    ok(just.map(times2).val() === 84, "Just.map");
-    ok(noth.map(times2) === Maybe.Nothing(), "Nothing.map");
-    ok(Maybe.unit(42).equals(just), "Maybe.unit");
+    ok("Just.map", just.map(times2).val() === 84);
+    ok("Nothing.map", noth.map(times2) === Maybe.Nothing());
+    ok("Maybe.unit", Maybe.unit(42).equals(just));
 
     function one (x) { return Maybe.Just(x + 1); }
     function two (x) { return Maybe.Just(x + 2); }
     function not (x) { return Maybe.Nothing() }
-    ok(just.then(one).then(two).val() === 45, "Bind with all Justs");
-    ok(just.then(one).then(not).then(two) === Maybe.Nothing(), "Bind with a Nothing in the middle");
+    ok("Bind with all Justs", just.then(one).then(two).val() === 45);
+    ok("Bind with a Nothing in the middle", just.then(one).then(not).then(two) === Maybe.Nothing());
   });
 
   suite("List", function () {
@@ -32,18 +32,18 @@ suite("Examples", function () {
     var fadd = function (a, x) { return a + x };
     var mtwo = function (x) { return x * 2 };
 
-    ok(List.fromArray([1, 2, 3]).equals(list), "List.fromArray");
-    ok(List(1, 2, 3).equals(list), "List.apply");
-    ok(list.foldl(fadd, 0) === 6, "List.foldl");
-    ok(list.length() === 3, "List.length");
+    ok("List.fromArray", List.fromArray([1, 2, 3]).equals(list));
+    ok("List.apply", List(1, 2, 3).equals(list));
+    ok("List.foldl", list.foldl(fadd, 0) === 6);
+    ok("List.length", list.length() === 3);
 
     var arr = list.toArray();
-    ok(arr.length === 3 && arr[0] === 1 && arr[1] === 2 && arr[2] === 3, "List.toArray");
-    ok(list.concat(List(4, 5, 6)).equals(List(1, 2, 3, 4, 5, 6)), "List.concat");
-    ok(List(list, List(4, 5, 6)).flatten().equals(List(1, 2, 3, 4, 5, 6)), "List.flatten");
-    ok(list.map(mtwo).equals(List(2, 4, 6)), "List.map");
-    ok(List.unit(42).equals(List(42)), "List.unit");
-    ok(list.then(function (x) { return List(x, x) })
-           .equals(List(1, 1, 2, 2, 3, 3)), "List bind");
+    ok("List.toArray", arr.length === 3 && arr[0] === 1 && arr[1] === 2 && arr[2] === 3);
+    ok("List.concat", list.concat(List(4, 5, 6)).equals(List(1, 2, 3, 4, 5, 6)));
+    ok("List.flatten", List(list, List(4, 5, 6)).flatten().equals(List(1, 2, 3, 4, 5, 6)));
+    ok("List.map", list.map(mtwo).equals(List(2, 4, 6)));
+    ok("List.unit", List.unit(42).equals(List(42)));
+    ok("List bind", list.then(function (x) { return List(x, x) })
+           .equals(List(1, 1, 2, 2, 3, 3)));
   });
 });
